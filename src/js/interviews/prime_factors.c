e83b6e35d9aabe6675f62753d80cacf59203196a
yuval.kariv <yukariv@microsoft.com>
2019-04-08 10:17:10 +0300

    Initial version of prime_factors.c

---
+++ src/js/interviews/prime_factors.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/* return a static string with the prime factors of num ',' seporated.
+ * Note: No chack for buffer overflow in return
+ */
+char *prime_factors(int num)
+{
+  int i = 2, e = sqrt((double)num)+1;
+  int f = 0;
+  static char buf[4096];
+  buf[0] = 0;
+  while (i < e)
+  {
+    int n = (num/i)*i;
+    if (n != num)
+    {
+      i++;
+      continue;
+    }
+    sprintf(buf+strlen(buf), "%s%d", f?",":"", i);
+    num = num/i;
+    f++;
+  }
+  if (!f)
+    sprintf(buf+strlen(buf), "%d", num);
+  return buf;
+}
+
+static void prime_factors_t(int n, char *e)
+{
+  char *rc = prime_factors(n);
+  if (!rc || strcmp(rc, e))
+  {
+    printf("ERR: prime_factors(%d): \"%s\" exp: \"%s\"\n", n, rc, e);
+    exit(1);
+  }
+  printf("prime_factors(%d): \"%s\"\n", n, rc);
+}
+
+int main(void)
+{
+  prime_factors_t(60, "2,2,3,5");
+  prime_factors_t(17, "17");
+  prime_factors_t(1, "1");
+  prime_factors_t(81, "3,3,3,3");
+  prime_factors_t(49, "7,7");
+  return 0;
+}
